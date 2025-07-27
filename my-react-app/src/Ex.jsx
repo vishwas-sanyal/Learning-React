dd4e2bbeee6f3d29e017a491351262a05d00f8e9
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-07-27 19:02:34 +0530

    Stopwatch

--- my-react-app/src/Ex.jsx
+++ my-react-app/src/Ex.jsx
@@ -1,18 +1,56 @@
-import React, { useState, createContext } from "react";
-import ExampleB from "./ExB";
+import React, { useState, useEffect, useRef } from "react";
 
-export const UserContext = createContext();
 
 function Example() {
 
-    const [user, setUser] = useState("Vishwas");
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
 
-    return (<div className="box">
-        <h1>BOX A</h1>
-        <h2>{`Hello ${user}`}</h2>
-        <UserContext.Provider value={user}>
-            <ExampleB user={user} />
-        </UserContext.Provider>
+    useEffect(() => {
+        if (isRunning) {
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10)
+        }
+        return () => {
+            clearInterval(intervalIdRef.current);
+        }
+    }, [isRunning]);
+    function start() {
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+    }
+    function stop() {
+        setIsRunning(false);
+    }
+    function reset() {
+        setElapsedTime(0);
+        setIsRunning(false);
+    }
+    function formatTime() {
+
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
+        return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+    }
+
+    return (<div className="stopwatch">
+        <div className="display">{formatTime()}</div>
+        <div className="controls">
+            <button className="start-btn" onClick={start}>Start</button>
+            <button className="stop-btn" onClick={stop}>Stop</button>
+            <button className="reset-btn" onClick={reset}>Reset</button>
+        </div>
     </div>);
 }
 
